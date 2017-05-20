e1809d9c9bf31e847147e9906a79d5407191fd68
Jean Petitclerc <jean.petitclerc@groupepp.com>
2017-05-19 23:01:00 -0400

    Exemple de fichier de config ajouté.

---
+++ app/config/config_example.py
@@ -0,0 +1,7 @@
+# Rename this file to config.py
+# Update with your own values
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data/aliments.db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+SECRET_KEY='VerySecretKey'
+DEBUG=True
